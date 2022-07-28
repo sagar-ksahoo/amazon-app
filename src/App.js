cba757b3e98e8f152f24bb14982626a5eb19a411
sagar-ksahoo <sagarsahoo1729@gmail.com>
2022-07-28 21:58:48 +0530

    home page created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import './App.css';
 import Header from './components/Header';
+import Home from './components/Home';
+
+
 
 function App() {
   return (
@@ -10,6 +13,8 @@ function App() {
       <Header/>
 
       {/* Home */}
+
+      <Home />
 
     </div>
   );
---
+++ src/components/Product.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import "./Product.css"
+
+const Product = ({title, price, image, rating}) => {
+  return (
+    <div className='product'>
+
+      <div className="product_info">
+        <p>{title}</p>
+        
+        <p className='product_price'>
+          <small>$</small>
+          <strong>{price}</strong>
+        </p>
+
+        <div className="product_rating">
+          {
+            Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>🌟</p>
+            ))
+          }
+          
+        </div>
+
+      </div>
+
+      <img 
+        src={image} 
+        alt="prod-img" 
+      />
+
+      <button>Add to Basket</button>
+
+    </div>
+  )
+}
+
+export default Product
